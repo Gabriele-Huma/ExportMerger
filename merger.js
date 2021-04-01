25820da6343a6cd3d544c55cc408f81af34ecc19
Gabriele-Huma <72381858+Gabriele-Huma@users.noreply.github.com>
2021-04-01 18:24:37 +0200

    Add CSV output
    CSV can be specified as third argument

--- merger.js
+++ merger.js
@@ -1,6 +1,15 @@
 const fs = require('fs')
 const path = require('path')
 
+function isArray(item) {
+    Object.prototype.toString.call(item) === '[object Array]'
+}
+
+function isObject(item)
+{
+    return item !== undefined && item !== null && item.constructor == Object;
+}
+
 // Merges the modules json files in a set of folders
 // where each folder usually corresponds to a user.
 // "Usually" means the code is not assuming that, it's using the 
@@ -46,23 +55,86 @@ function merge(folders) {
 }
 
 // Writes ddown a set of data structures into correspondent files in a given folder
-function store(mergedFiles, outputFolder) {
-    console.log(`\nConsolidating merged files to ${outputFolder}`)
+function store(mergedFiles, outputFolder, outputType) {
+    console.log(`\nConsolidating merged files to ${outputFolder} in ${outputType} format`)
     if(!fs.existsSync(outputFolder))
         fs.mkdirSync(outputFolder)
         
-    for(module of Object.keys(mergedFiles)) 
-        fs.writeFileSync(`${outputFolder}/${module}.json`, JSON.stringify(mergedFiles[module]))
+    // Convert to CSV if specified
+    if(outputType.toLowerCase() === 'csv') {
+        for(module of Object.keys(mergedFiles))      
+            fs.writeFileSync(`${outputFolder}/${module}.csv`, toCSV(mergedFiles[module]))
+    }
+    else {
+        for(module of Object.keys(mergedFiles)) 
+            fs.writeFileSync(`${outputFolder}/${module}.json`, JSON.stringify(mergedFiles[module]))
+    }
+}
+
+// Flat the struccture of a json
+// { key1: { key2: v1, key3: v2 }} becomes { key1.key2: v1, key1.key3: v2 }
+function flat(keyPrefix, json) {
+    // Recursively dig into the object structure
+    if(isArray(json)) {
+        var res = {}
+        for(var i = 0; i < json.length; i++) {
+            let item = json[i]
+            let flatten = flat(`${i}`, item)
+            for(subkey of Object.keys(flatten)) 
+                res[`${keyPrefix}.${subkey}`] = flatten[subkey]
+        }
+        return res
+    } 
+    else if(isObject(json)) {
+        var res = {}
+        for(key of Object.keys(json)) {
+            let flatten = flat(`${key}`, json[key])
+            for(subkey of Object.keys(flatten)) 
+                res[`${keyPrefix}.${subkey}`] = flatten[subkey]
+        }
+        return res
+    }
+    else {
+        var res = {}
+        res[keyPrefix] = json
+        return res
+    }
+}
+
+// Converts JSON to CSV
+function toCSV(moduleContent) {    
+    // First, flat json structure of each module
+    let flatten = moduleContent.map(e => flat("root", e))
+    
+    // Second, for extra safety let's just callect all the 'columns' of all the entries
+    // Just in case some entry has more colums than another
+    var columns = []
+    flatten.forEach(entry => Object.keys(entry).forEach(col => {
+        if(!columns.includes(col))
+            columns.push(col)
+    }))
+
+    // Now following the order of the columns we create the CSV header, removing the 'root.' prefix
+    let csvHeader = columns.map(c => c.replace('root.', '')).join(";")
+
+    // And then the content, for each entry
+    let csvContent = flatten.map(entry => columns.map(c => entry[c] ? entry[c] : "").join(";"))
+
+    // Finally return the concatenation of header and content
+    return csvHeader + "\n" + csvContent.join("\n") 
 }
 
 // Main
 let args = process.argv.slice(2)
 let rootFolder = args[0]
 let outputFolder = args[1]
+let outputType = args[2]
 if(!rootFolder || !outputFolder) {
-    console.log("--- Usage ---\nnode merger.js <inputFolder> <outputFolder>\n--- ----- ---")
+    console.log("--- Usage ---\nnode merger.js <inputFolder> <outputFolder> <csv/json>\n--- ----- ---")
     return
 }
+if(!outputType)
+    outputType = "json"
 
 // Check folder exists
 try {
@@ -76,10 +148,9 @@ try {
         let mergedFiles = merge(folders)   
 
         // Consolidate
-        store(mergedFiles, outputFolder)
+        store(mergedFiles, outputFolder, outputType)
     }
     else 
         console.log(`The folder ${rootFolder} does not exist`)
 } catch(err) {
 }
-
